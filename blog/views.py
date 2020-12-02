d3c976338f41b1121bc35340dd4af909d8e9a91d
Asif Mohammad Mollah <admin@mrasif.in>
2020-12-03 00:58:43 +0530

    chore: serializer created

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,18 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from blog.models import Blog
+from blog.serializers import BlogSerializer
 
 
 class BlogListAPIView(GenericAPIView):
+
+    serializer_class = BlogSerializer
 
     def get_queryset(self):
         return Blog.objects.all()
 
     def get(self, request, *args, **kwargs):
         blogs = self.get_queryset()
+        serializer = self.get_serializer(blogs, many=True)
         print(blogs)
-        return Response(data={}, status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
